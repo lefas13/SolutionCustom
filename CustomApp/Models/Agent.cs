e5e07b1973a01a05e175a03c5bcb2ec7ed5cfaed
lefas_13 <lefas_13@A505-24>
2024-10-01 10:56:53 +0300

    Добавьте файлы проекта.

---
+++ CustomApp/Models/Agent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomApp.Models
+{
+    public class Agent
+    {
+        public int AgentId { get; set; }
+        public string FIO { get; set; }
+        public string IDNumber { get; set; }
+    }
+}
